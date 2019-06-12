4284648a15a9a4ded4a76c585194396f4759a8a7
y2k <itwisterlx@gmail.com>
2019-06-12 14:17:07 +0300

    fixed #1

--- client/src/main/java/y2k/backendui/client/MainActivity.kt
+++ client/src/main/java/y2k/backendui/client/MainActivity.kt
@@ -3,17 +3,18 @@ package y2k.backendui.client
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import okhttp3.*
+import okio.ByteString
 import y2k.virtual.ui.*
 import java.io.ByteArrayInputStream
 import java.io.ObjectInputStream
-import java.net.HttpURLConnection
-import java.net.URL
 import java.util.concurrent.CountDownLatch
-import kotlin.concurrent.thread
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var virtualHostView: VirtualHostView
+
+    private lateinit var socket: WebSocket
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,18 +25,36 @@ class MainActivity : AppCompatActivity() {
 
     override fun onStart() {
         super.onStart()
-
-        fun reloadNode(msg: String?) {
-            thread {
-                val node = attachClickListeners(downloadNode(msg), ::reloadNode)
-                sendNodeToUI(node)
-            }
-        }
-
-        reloadNode(null)
+        startSocket()
     }
 
-    private fun attachClickListeners(node: VirtualNode, f: (String) -> Unit): VirtualNode {
+    private fun startSocket() {
+        val request = Request.Builder()
+            .url("ws://192.168.0.100:8080/socket")
+            .build()
+
+        socket = OkHttpClient.Builder()
+            .build()
+            .newWebSocket(request, object : WebSocketListener() {
+
+                override fun onOpen(webSocket: WebSocket, response: Response) {
+                    webSocket.send(":CONNECT:")
+                }
+
+                override fun onMessage(webSocket: WebSocket, bytes: ByteString) {
+                    bytes
+                        .toByteArray()
+                        .let(::ByteArrayInputStream)
+                        .let(::ObjectInputStream)
+                        .readObject()
+                        .let { it as VirtualNode }
+                        .let { attachClickListeners(it, webSocket::send) }
+                        .let(::sendNodeToUI)
+                }
+            })
+    }
+
+    private fun attachClickListeners(node: VirtualNode, f: (String) -> Any?): VirtualNode {
         if (node !is View_) return node
 
         val msgProp = node.props.find { it.name == "contentDescription" }
@@ -64,21 +83,6 @@ class MainActivity : AppCompatActivity() {
         return node
     }
 
-    private fun downloadNode(msg: String?): VirtualNode {
-        val url = URL("http://192.168.0.100:8080/")
-
-        val conn = url.openConnection() as HttpURLConnection
-
-        if (msg != null)
-            conn.addRequestProperty("RemoteUI-Msg", msg)
-
-        return conn.inputStream
-            .readBytes()
-            .let(::ByteArrayInputStream)
-            .let(::ObjectInputStream)
-            .readObject() as VirtualNode
-    }
-
     private fun sendNodeToUI(node: VirtualNode) {
         val cdl = CountDownLatch(1)
         runOnUiThread {
@@ -92,5 +96,10 @@ class MainActivity : AppCompatActivity() {
             cdl.countDown()
         }
         cdl.await()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        socket.close(0, null)
     }
 }
